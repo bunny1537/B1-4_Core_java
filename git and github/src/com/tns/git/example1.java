cb6716b007bd564cd083396f1cd569014567ef55
Banashree s dalawai <banashreesdalawai98@gmail.com>
2022-03-29 18:32:06 +0530

    First commit

---
+++ git and github/src/com/tns/git/example1.java
@@ -0,0 +1,11 @@
+package com.tns.git;
+
+public class example1 {
+
+	public static void main(String[] args) {
+		System.out.println("akansha singh");
+		System.out.println("Banashree");
+
+	}
+
+}
---
+++ git and github/src/com/tns/git/example2.java
@@ -0,0 +1,17 @@
+package com.tns.git;
+
+public class example2 {
+
+	public static void main(String[] args) {
+		int num1 = 5, num2 = 15, sum,total,remainder;
+	      sum = num1 + num2;
+	      total = num1 * num2;
+	      remainder = num1 - num2;
+
+	      System.out.println("Sum of these numbers: "+sum);
+	      System.out.println("multiply of these numbers: "+total);
+	      System.out.println("subtraction of thses numbers: "+remainder);
+	      
+	}
+
+}
