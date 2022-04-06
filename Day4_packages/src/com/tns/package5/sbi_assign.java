8e22858e3402ea7da9435dffcbfbb104a73f5813
Banashree s dalawai <banashreesdalawai98@gmail.com>
2022-04-06 13:45:11 +0530

    first commit

---
+++ Day4_packages/src/com/tns/package5/sbi_assign.java
@@ -0,0 +1,17 @@
+package com.tns.package5;
+
+public class sbi_assign 
+{
+  private int balance;
+  public sbi_assign()
+  {
+  }
+  public int getBalance()
+  {
+	  return balance;
+  }
+  public void setBalance(int bal)
+  {
+	  balance=bal;
+  }
+}
---
+++ Day4_packages/src/com/tns/package6/encapsulation_assign.java
@@ -0,0 +1,13 @@
+package com.tns.package6;
+import com.tns.package5.sbi_assign;
+public class encapsulation_assign 
+{
+
+	public static void main(String[] args) 
+	{
+	com.tns.package5.sbi_assign sb=new sbi_assign();
+	sb.setBalance(24500);
+    System.out.print("your SB A/c balance:"+sb.getBalance());;
+	}
+
+}
