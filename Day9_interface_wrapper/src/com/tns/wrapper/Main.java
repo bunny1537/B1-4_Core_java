14e8ef92cba54e010d532c59e3d66e1111193274
Banashree s dalawai <banashreesdalawai98@gmail.com>
2022-04-08 10:19:51 +0530

    first commit

---
+++ Day9_interface_wrapper/src/com/tns/wrapper/Main.java
@@ -0,0 +1,14 @@
+package com.tns.wrapper;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		
+		Character ch='a';
+		char a =ch;
+		System.out.println("character ch: "+ch);
+		System.out.println("char a: "+a);
+	}
+
+}
